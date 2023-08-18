529ca8696038913f892aec8858a86c9d8097c416
OTH-BD <othmane.boudali01@gmail.com>
2023-08-18 20:36:52 +0100

    'Weatherapi'

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+
+    weatherApiBaseUrl:'https://api.openweathermap.org/data/2.5/weather',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'46d3778373140038b1f37caec74755b4',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue:'weather-api99.p.rapidapi.com'
+  
+
+};
